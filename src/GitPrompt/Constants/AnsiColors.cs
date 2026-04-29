d381fc2238e72747b1cbd438a5c953608c7fbddd
Eduardo Quintana <eduardo.quintana@supergroup.com>
2026-04-29 23:48:35 +0200

    Update ansi colors

--- src/GitPrompt/Constants/AnsiColors.cs
+++ src/GitPrompt/Constants/AnsiColors.cs
@@ -2,14 +2,14 @@ namespace GitPrompt.Constants;
 
 internal static class AnsiColors
 {
-    internal const string Green     = "#00BB00";
-    internal const string Magenta   = "#CB06B2";
-    internal const string Orange    = "#D78700";
-    internal const string Red       = "#CC0000";
-    internal const string BoldRed   = "#FF5555";
+    internal const string Green = "#13A10E";
+    internal const string Magenta = "#B20F9D";
+    internal const string Orange = "#D78700";
+    internal const string Red = "#CC0000";
+    internal const string BoldRed = "#FF5555";
     internal const string LightGray = "#AAAAAA";
-    internal const string Yellow    = "#FFA002";
-    internal const string Blue      = "#48A8CD";
+    internal const string Yellow = "#FFA002";
+    internal const string Blue = "#48A8CD";
 
     internal const string Reset = "\e[0m";
 }
